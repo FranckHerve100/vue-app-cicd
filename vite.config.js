f1840f23b01629a346be85611c85e88ccfd2636f
FranckHerve100 <franckytchana08@gmail.com>
2025-01-20 15:46:52 +0100

    Github Actionsv2

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 /// <reference types="node" />
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173, // Utilise le port défini par Heroku ou 5173 par défaut
-    host: true, // Assure que Vite écoute sur 0.0.0.0, nécessaire pour le déploiement sur Heroku
+    port: process.env.PORT || 5173,
+    host: true, // Permet d'écouter sur toutes les interfaces
+    allowedHosts: ['vue-app-cicd-1-2fdfe77ae104.herokuapp.com'], // Ajoute l'hôte Heroku autorisé
   }
 })
