da21aea23105e44a16027441edf347424ff025a2
FranckHerve100 <franckytchana08@gmail.com>
2025-01-20 15:39:44 +0100

    Github Actionsv2

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.VITE_PORT || 5173 // Utilise le port défini par Heroku ou 5173 par défaut
+    port: process.env.PORT || 5173, // Utilise le port défini par Heroku ou 5173 par défaut
+    host: true, // Assure que Vite écoute sur 0.0.0.0, nécessaire pour le déploiement sur Heroku
   }
 })
