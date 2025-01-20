50fd9861bf5d60fa42620b12781409ed43c4bf4f
FranckHerve100 <franckytchana08@gmail.com>
2025-01-20 12:21:13 +0100

    Github Actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: import.meta.env.VITE_PORT || 5173 // Utilise le port défini par Heroku ou 5173 par défaut
+  }
 })
