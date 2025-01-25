ef6d0d4317e88e8e94f5f92d632d245dfb7c6565
FranckHerve100 <franckytchana08@gmail.com>
2025-01-25 20:52:19 +0100

    Github Actionsvf1

--- src/pages/Acceuil.jsx
+++ src/pages/Acceuil.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom"
 function Acceuil () {
     return (
         <div className="container flex flex-col h-screen justify-center items-center">
-            <h1 className="text-2xl">Merci de deposer votre Avis</h1>
-            <Link to={'/deposer-un-avis'} className="text-xl text-red-500 border-b border-green-500">
+            <h1 className="text-2xl">Merci de deposer votre Avis Madame/Monsieur</h1>
+            <Link to={'/deposer-un-avis'} className="text-xl text-green-500 border-b border-green-500">
                 Nouvel avis
             </Link>
         </div>
